b72c25002fb238cc637dd009459ca7f5f6aa6a91
Fernando Montero <fernando.montero.perez@gmail.com>
2020-12-01 10:03:34 +0000

    Cleanup

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -2,34 +2,34 @@ import { data } from './data';
 
 const result = data.split(/\r\n|\r|\n/g).map(i => Number(i));
 
-const pairs: number[][] = [];
+let pairs: [number, number] = [0, 0];
 
 result.forEach((entry1, index) => {
   result.slice(index + 1).forEach(entry2 => {
     if (entry1 + entry2 === 2020) {
-      pairs.push([entry1, entry2]);
+      pairs = [entry1, entry2];
     }
   });
 });
 
 export const part1 = {
-  pairs: pairs[0],
-  sum: pairs[0][0] * pairs[0][1],
+  pairs,
+  sum: pairs[0] * pairs[1],
 };
 
-const triples: number[][] = [];
+let triples: [number, number, number] = [0, 0, 0];
 
 result.forEach((entry1, index) => {
   result.slice(index + 1).forEach(entry2 => {
     result.slice(index + 2).forEach(entry3 => {
       if (entry1 + entry2 + entry3 === 2020) {
-        triples.push([entry1, entry2, entry3]);
+        triples = [entry1, entry2, entry3];
       }
     });
   });
 });
 
 export const part2 = {
-  triples: triples[0],
-  sum: triples[0][0] * triples[0][1] * triples[0][2],
+  triples,
+  sum: triples[0] * triples[1] * triples[2],
 };
